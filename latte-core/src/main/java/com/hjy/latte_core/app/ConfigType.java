6197a4464b905186c877e60dbf21440a3ffc5c4b
HeJiangYue <HeJiangYue>
2019-12-10 18:03:27 +0800

    初始化一些config

---
+++ latte-core/src/main/java/com/hjy/latte_core/app/ConfigType.java
@@ -0,0 +1,9 @@
+package com.hjy.latte_core.app;
+
+public enum ConfigType {
+
+    API_HOST,               //配置请求的域名
+    APPLICATION_CONTEXT,    //全局上下文
+    CONFIG_READY,           //控制初始化是否完成
+    ICON                    //存储一些字体的初始化项目
+}
---
+++ latte-core/src/main/java/com/hjy/latte_core/app/Latte.java
@@ -0,0 +1,9 @@
+package com.hjy.latte_core.app;
+
+/**
+ * 对外的工具类
+ */
+public final class Latte {
+
+
+}
